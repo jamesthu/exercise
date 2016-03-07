903db5fba2ddb1e074de285d72b4eb64d798fd83
s0239 <18810918553@163.com>
2016-03-07 14:20:26 +0800

    add freelist

--- 5.1_first.cpp
+++ 5.1_first.cpp
@@ -13,10 +13,13 @@ struct block
 	int size;
 	struct block *last;
 	struct block *next;
+	struct block *lastFree;
+	struct block *nextFree;
 	bool flag;
 };
 
 block *base;
+block *freeBase;
 
 void blockInit(){
 	base = new block();
@@ -24,7 +27,17 @@ void blockInit(){
 	base->size = MAX;
 	base->last = NULL;
 	base->next = NULL;
+	base->lastFree = NULL;
+	base->nextFree = NULL;
 	base->flag = false;
+	freeBase = new block();
+	freeBase->startAddress = -1;
+	freeBase->size = -1;
+	freeBase->last = NULL;
+	freeBase->next = NULL;
+	freeBase->lastFree = NULL;
+	freeBase->nextFree = base;
+	base->lastFree = freeBase;
 }
 
 void blockSplit(block *head, int num){
@@ -45,28 +58,40 @@ void blockSplit(block *head, int num){
 		myBlock->last = head;
 		head->flag = true; 
 		myBlock->flag = false;
+		myBlock->lastFree = head->lastFree;
+		if(myBlock->lastFree != NULL){
+			myBlock->lastFree->nextFree = myBlock;
+		}
+		myBlock->nextFree = head->nextFree;
+		if(myBlock->nextFree != NULL)
+			myBlock->nextFree->lastFree = myBlock;
+		head->lastFree = head->nextFree = NULL;
 	}
 }
 
 void *blockMalloc(int num){
-	block *head = base;
+	block *head;
+	if(freeBase->nextFree != NULL){
+		head = freeBase->nextFree;
+	}
+	else{
+		return NULL;
+	}
 	while(1){
-		if(!head->flag){
-			if(head->size >= num){
-				blockSplit(head, num);
-				return head;
+		if(head->size >= num){
+			blockSplit(head, num);
+			return head;
+		}
+		else{
+			if(head->nextFree != NULL){
+				head = head->nextFree;
 			}
 			else{
-				if(head->next != NULL){
-					head = head->next;
-				}
-				else{
-					return NULL;
-				}
+				return NULL;
 			}
 		}
-		if(head->next != NULL){
-			head = head->next;
+		if(head->nextFree != NULL){
+			head = head->nextFree;
 		}
 		else{
 			return NULL;
@@ -75,23 +100,56 @@ void *blockMalloc(int num){
 }
 
 void blockFree(void *p){
+	block *freeHead = freeBase->nextFree;
 	block *head = (block *)p;
 	head->flag = false;
-	if(head->last != NULL && head->last->flag == false){
-		head = head->last;
-		head->size += head->next->size;
-		head->next = head->next->next;
-		if(head->next != NULL){
-			head->next->last = head;
+	while(freeHead){
+		if(freeHead->startAddress < head->startAddress){
+			if(freeHead->nextFree != NULL){
+				if(freeHead->nextFree->startAddress < head->startAddress){
+					freeHead = freeHead->nextFree;
+					continue;
+				}
+				else
+					break;
+			}
+			else
+				break;
+		}
+		else{
+			freeHead = freeHead->lastFree;
+			break;
 		}
 	}
-	if(head->next != NULL && head->next->flag == false){
-		head->size += head->next->size;
-		head->next = head->next->next;
-		if(head->next != NULL){
-			head->next->last = head;
+	if(!freeHead){
+		freeBase->nextFree = head;
+		head->lastFree = freeBase;
+		head->nextFree = NULL;
+	}
+	else{		
+		if(freeHead->startAddress+freeHead->size == head->startAddress){
+			head = head->last;
+			head->size = head->size + head->next->size;
+			head->next = head->next->next;
+			if(head->next != NULL)
+				head->next->last = head;
+		}
+		else{
+			head->nextFree = freeHead->nextFree;
+			head->lastFree = freeHead;
+			freeHead->nextFree = head;
+		}
+		if(head->nextFree != NULL && head->startAddress + head->size == head->nextFree->startAddress){
+			head->nextFree = head->nextFree->nextFree;
+			if(head->nextFree != NULL)
+				head->nextFree->lastFree = head;
+			head->size = head->size + head->next->size;
+			head->next = head->next->next;
+			if(head->next != NULL)
+				head->next->last = head;
 		}
 	}
+
 }
 
 void blockPrint(){
